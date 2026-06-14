645e8e3ffc9402424fa05b70ae6e362fbe645e30
Erdogan Tan <124918396+turkishrational@users.noreply.github.com>
2026-06-14 22:14:10 +0300

    Delete trdos386_libc_project_2026/gcc127 directory

--- trdos386_libc_project_2026/gcc127/build_trdos.bat
+++
@@ -1,3 +0,0 @@
-:: -Ttext 0x0 ile ORG 0 yapýlýr ve linker otomatik olarak _end deðerini belirler
-
-ld -o GCC127.PRG crt0.o compiler.o open.o close.o read.o write.o sbrk.o --oformat binary -Ttext 0x0
