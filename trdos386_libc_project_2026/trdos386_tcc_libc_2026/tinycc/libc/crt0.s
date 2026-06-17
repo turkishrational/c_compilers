f16844a85f279073d367d412af63dad92345b55f
Erdogan Tan <124918396+turkishrational@users.noreply.github.com>
2026-06-17 04:09:27 +0300

    Add files via upload

--- trdos386_libc_project_2026/trdos386_tcc_libc_2026/tinycc/libc/crt0.s
+++ trdos386_libc_project_2026/trdos386_tcc_libc_2026/tinycc/libc/crt0.s
@@ -9,10 +9,15 @@ _start:
     .ascii "CRT0"
 
 .L_START:
+    .extern _end                    /* from 'trdos386.ld' linker script ... end of bss */
+    mov ebx, offset _end            /* set new u.break address to end of bss */
+    mov eax, 17                     /* sysbreak ... kernel clears memory space */
+    int 0x40                        /*  between the new (_end) and old u.break address */
+
     /* esp points to argc directly */
     pop eax                         /* eax = argc (value) */
     mov ebx, esp                    /* ebx = argv[0] pointer address (not value) */
-    
+
     push ebx                        /* Pass argv pointer to main (Argument 2) */
     push eax                        /* Pass argc value to main (Argument 1) */
 
@@ -24,10 +29,10 @@ _start:
     
     /* System exit routine via TRDOS 386 Kernel */
     mov ebx, eax                    /* Forward main's exit code to EBX for TRDOS */
-    mov eax, 1                      /* TRDOS sys_exit system call number *
+    mov eax, 1                      /* TRDOS sys_exit system call number */
     int 0x40                        /* Call TRDOS 386 Kernel */
 
-    .byte 0	
+    .byte 0
     .ascii "C Compiler v1.0 for TRDOS 386"
     .byte 0
     .ascii "Erdogan Tan - 2026"
