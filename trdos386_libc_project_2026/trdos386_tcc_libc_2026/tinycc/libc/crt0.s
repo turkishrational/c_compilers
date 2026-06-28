ecfe6d5154a9f5ab9821b597d06a0035c2edcb6f
Erdogan Tan <124918396+turkishrational@users.noreply.github.com>
2026-06-29 01:21:10 +0300

    Add files via upload

--- trdos386_libc_project_2026/trdos386_tcc_libc_2026/tinycc/libc/crt0.s
+++ trdos386_libc_project_2026/trdos386_tcc_libc_2026/tinycc/libc/crt0.s
@@ -4,30 +4,34 @@
 .text
 
 _start:
-    jmp .L_START                    /* TRDOS 386 PRG execution entry jump */
+    jmp .L_START
 
     .ascii "CRT0"
 
 .L_START:
-    /* esp points to argc directly */
-    pop eax                         /* eax = argc (value) */
-    mov ebx, esp                    /* ebx = argv[0] pointer address (not value) */
+    .extern _end
+
+    mov ebx, offset _end            /* ebx = Nihai BSS sonu adresi */
+    add ebx, 3
+    and ebx, 0xFFFFFFFC             /* 4-Byte Dword Hizalama */
+    mov eax, 17                     /* EAX = 17 (sys_break) */
+    int 0x40                        /* TRDOS Kernel Resmi Bildirimi */
+
+    /* =========================================================================
+       ?? [ANA KODA GEÇÝÞ]
+       ========================================================================= */
+    pop eax                         /* eax = argc */
+    mov ebx, esp                    /* ebx = argv pointer */
+    push ebx
+    push eax
+    call _main                      /* TCC Ana Derleyici Motoru */
     
-    push ebx                        /* Pass argv pointer to main (Argument 2) */
-    push eax                        /* Pass argc value to main (Argument 1) */
+    add esp, 8
+    mov ebx, eax
+    mov eax, 1                      /* sys_exit */
+    int 0x40
 
-    call _main                      /* Execute main function of TCC/program */
-
-    /* At this point, main's return/exit code is inside EAX register */
-
-    add esp, 8                      /* Clean up the 2 arguments from stack */
-    
-    /* System exit routine via TRDOS 386 Kernel */
-    mov ebx, eax                    /* Forward main's exit code to EBX for TRDOS */
-    mov eax, 1                      /* TRDOS sys_exit system call number */
-    int 0x40                        /* Call TRDOS 386 Kernel */
-
-    .byte 0	
+    .byte 0
     .ascii "C Compiler v1.0 for TRDOS 386"
     .byte 0
     .ascii "Erdogan Tan - 2026"
