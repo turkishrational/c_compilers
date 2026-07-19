56f3d628eec36e8b721b65b5ebc5bd775042250a
Erdogan Tan <124918396+turkishrational@users.noreply.github.com>
2026-07-20 01:57:35 +0300

    Add files via upload

---
+++ tinycc/diskd_tcc/libc.h
@@ -0,0 +1,8 @@
+/* libc.h - TRDOS 386 Native Toolchain Test Header */
+#ifndef _LIBC_H
+#define _LIBC_H
+
+// Extern bildirimini header içine gizliyoruz
+extern int printf(const char *format, ...);
+
+#endif
